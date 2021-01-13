c28bd0dbe00c00363edabec7b05766bac1f15b2b
Raul Morales <rhaul270795@gmail.com>
2021-01-12 22:07:55 -0300

    ejer1

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+#esto es un comentario
+print("hola mundo")
+
+#variables
+palabra1 = 11
+texto1 ="hola hola"
+mayordeedad = True
+
+#tipo de variable
+print("esta variable es de tipo",type(texto1))
+print("esta variable es de tipo",type(mayordeedad))
