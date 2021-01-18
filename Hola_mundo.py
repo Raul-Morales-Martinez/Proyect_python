04604bb88da03eecf7213ac9256e5a93f4925106
Raul Morales <rhaul270795@gmail.com>
2021-01-18 20:16:37 -0300

    hola mundo, concatenar

--- Hola_mundo.py
+++ Hola_mundo.py
@@ -1,15 +1,16 @@
 # 1. TAREA: imprimir "Hola mundo"
-print( your code here )
+
+print( "Hola Mundo" )
 # 2. imprimir "Hola Noelle!" con el nombre en una variable
 name = "Noelle"
-print( your code here )	# con una coma
-print( your code here )	# con un +
+print( "Hola",name,"42!")	# con una coma
+print( "Hola"+name+"42!")	# con un +
 # 3. imprimir "Hola 42!" con un numero en una variable
 name = 42
-print( your code here )	# con una coma
-print( your code here )	# con un + - !Este debería darnos un error!
+print( "hola",name)	# con una coma
+#print( "hola"+name)	# con un + - !Este debería darnos un error!
 # 4. imprimir "Me encanta comer sushi and pizza." con los alimentos en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print( your code here ) # con .format()
-print( your code here ) # con una cadena f
+print( "Me encanta comer {} y {} ".format(fave_food1,fave_food2)) # con .format()
+print( f"Me encanta comer {fave_food1} y {fave_food2}" ) # con una cadena f
