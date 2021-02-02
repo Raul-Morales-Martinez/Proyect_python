82474da0e7ab08ede7769a1c8a39be9e3b1ad9f1
Raul Morales <rhaul270795@gmail.com>
2021-02-02 18:52:40 -0300

    Creacion de instancias en el oop de python

---
+++ Usuario_oop.py
@@ -0,0 +1,42 @@
+
+class User:		# here's what we have so far
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+    # adding the deposit method
+    def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
+    	self.account_balance += amount	# the specific user's account increases by the amount of the value received
+    def make_withdrawal (self, cantidad):
+        self.account_balance -= cantidad
+    def display_user_balance (self):
+        print("Usuario:",self.name,",","saldo: $",self.account_balance)
+    def transfer_money (self, other_user, cantidadd) :
+        self.account_balance -= cantidadd
+        other_user.make_deposit(cantidadd)
+
+guido = User ("Guido van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+
+guido.make_deposit(100)
+guido.make_deposit(200)
+guido.make_deposit(150)
+guido.make_withdrawal(50)
+guido.display_user_balance()
+
+monty.make_deposit(50)
+monty.make_deposit(100)
+monty.make_withdrawal(10)
+monty.make_withdrawal(15)
+monty.display_user_balance()
+
+jose = User("Jose de las mercedez","josecito@python.com")
+jose.make_deposit(1000)
+jose.make_withdrawal(100)
+jose.make_withdrawal(150)
+jose.make_withdrawal(200)
+jose.display_user_balance()
+
+jose.transfer_money(monty,200)
+jose.display_user_balance()
+monty.display_user_balance()
---
+++ intro_oop.py
@@ -0,0 +1,50 @@
+class User:		# declare a class and give it name User
+    def __init__(self):
+        self.name = "Michael"
+        self.email = "michael@codingdojo.com"
+        self.account_balance = 0
+guido = User()
+monty = User()
+
+#Si queremos acceder a los atributos de nuestra instancia, podemos referirnos a ellos desde nuestras instancias por nombre
+print(guido.name)	# output: Michael
+print(monty.name)	# output: Michael
+
+#También podemos establecer los valores para los atributos de nuestra instancia
+guido.name = "Guido"
+monty.name = "Monty"
+
+#Con los parámetros. del método__init__  , indicamos lo que se debe proporcionar (es decir, argumentos )
+# cuando se instancia la clase. ( selfsiempre se pasa implícitamente)
+
+class User:
+    def __init__(self, username, email_address):# now our method has 2 parameters!
+        self.name = username			# and we use the values passed in to set the name attribute
+        self.email = email_address		# and the email attribute
+        self.account_balance = 0		# the account balance is set to $0, so no need for a third parameter
+
+guido = User("Guido van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+print(guido.name)	# output: Guido van Rossum
+print(monty.name)	# output: Monty Python
+print(guido.email)
+print(guido.account_balance)
+
+###
+class Userr:		# here's what we have so far
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+    # adding the deposit method
+    def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
+    	self.account_balance += amount	# the specific user's account increases by the amount of the value received
+
+guido = Userr ("Guido van Rossum", "guido@python.com")
+monty = Userr("Monty Python", "monty@python.com")
+guido.make_deposit(100)
+guido.make_deposit(200)
+monty.make_deposit(50)
+print(guido.account_balance)	# output: 300
+print(monty.account_balance)	# output: 50
+print(guido.name)
